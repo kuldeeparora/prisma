80de01369836897fd89e0b032a4ec9a082e928ea
Kuldeep Arora <kuldeep.arora@virginmedia.co.uk>
2020-06-24 22:46:20 +0100

    starter file

---
+++ starter/script.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+// A `main` function so that you can use async/await
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const post = await prisma.post.create({
+    data: {
+      title: "Prisma makes databases easy",
+      author: {
+        connect: { email: "sarah@prisma.io" },
+      },
+    },
+  })
+  console.log(post)
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.disconnect()
+  })
